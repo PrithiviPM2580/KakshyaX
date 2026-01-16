b5056e111dcdd4e36c8017bb37dc79ce8bd557fd
Prithivi <prithivi@users.noreply.github.com>
2026-01-17 00:24:28 +0545

    Implement authentication library with better-auth and MongoDB adapter

---
+++ backend/src/lib/auth.lib.ts
@@ -0,0 +1,28 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { getDatabaseConnection } from "@/config/db.config.js";
+import APIError from "./api-error.lib.js";
+import config from "@/config/env.config.js";
+
+const client = getDatabaseConnection();
+
+if (!client.db) {
+  throw new APIError(500, "Database connection not established");
+}
+
+export const auth = betterAuth({
+  database: mongodbAdapter(client.db),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: config.GOOGLE_CLIENT_ID,
+      clientSecret: config.GOOGLE_CLIENT_ID,
+      redirectURI: config.GOOGLE_REDIRECT_URI,
+    },
+  },
+  secret: config.BETTER_AUTH_SECRET,
+  baseURL: config.BETTER_AUTH_URL,
+  basePath: "/api/v1/auth",
+});
