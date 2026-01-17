ef3b32391c9f38e1b93dd2a112abe83865f6e291
Prithivi <prithivi@users.noreply.github.com>
2026-01-18 05:30:53 +0545

    Implement user retrieval by ID: add controller for fetching user details by ID

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,8 +1,14 @@
 import { getValidatedPart, successResponse } from "@/utils/index.util.js";
 import { logger } from "better-auth";
 import type { Request, Response, NextFunction } from "express";
-import { getAllUserService } from "@/services/user.service.js";
-import type { GetAllUserInput } from "@/validator/user.validator.js";
+import {
+  getAllUserService,
+  getUserByIdService,
+} from "@/services/user.service.js";
+import type {
+  GetUserByIdInput,
+  GetAllUserInput,
+} from "@/validator/user.validator.js";
 
 export async function getAllUsersController(
   req: Request,
@@ -16,4 +22,18 @@ export async function getAllUsersController(
   logger.info(`Fetched ${users} users from database`);
 
   successResponse(res, 200, "Users fetched successfully", { users });
+}
+
+export async function getUserByIdController(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const params = getValidatedPart<GetUserByIdInput>(req, "params");
+
+  const user = await getUserByIdService(params);
+
+  logger.info(`Fetched user with id ${params.id} from database`);
+
+  successResponse(res, 200, "User fetched successfully", { user });
 }
