7c7610de1b2b6f36c286ef61661a033f8fb8219f
Prithivi <prithivi@users.noreply.github.com>
2026-01-17 18:53:43 +0545

    Add user route: implement GET endpoint for retrieving all users with authentication and rate limiting

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,5 +1,17 @@
+import { getAllUsersController } from "@/controllers/user.controller.js";
+import asyncHandler from "@/middlewares/async-handler.middleware.js";
+import { authenticateMiddleware } from "@/middlewares/authenticate.middleware.js";
+import { apiLimitter } from "@/middlewares/rate-limit.middleware.js";
 import { Router } from "express";
 
 const router: Router = Router();
+
+router
+  .route("/")
+  .get(
+    apiLimitter,
+    authenticateMiddleware(["admin", "teacher"]),
+    asyncHandler(getAllUsersController),
+  );
 
 export default router;
