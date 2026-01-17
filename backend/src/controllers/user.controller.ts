14c6e51839af1ecedeb4a6583320d32ea957441c
Prithivi <prithivi@users.noreply.github.com>
2026-01-18 05:18:37 +0545

    Implement getAllUsersController: retrieve and respond with all users from the database

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,7 +1,19 @@
+import { getValidatedPart, successResponse } from "@/utils/index.util.js";
+import { logger } from "better-auth";
 import type { Request, Response, NextFunction } from "express";
+import { getAllUserService } from "@/services/user.service.js";
+import type { GetAllUserInput } from "@/validator/user.validator.js";
 
 export async function getAllUsersController(
   req: Request,
   res: Response,
   next: NextFunction,
-) {}
+) {
+  const query = getValidatedPart<GetAllUserInput>(req, "query");
+
+  const users = await getAllUserService(query);
+
+  logger.info(`Fetched ${users} users from database`);
+
+  successResponse(res, 200, "Users fetched successfully", { users });
+}
