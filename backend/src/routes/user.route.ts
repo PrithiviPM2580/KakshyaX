96640cdb265f7104e4ab445c303490289587f1fd
Prithivi <prithivi@users.noreply.github.com>
2026-01-18 05:18:30 +0545

    Add request validation middleware: implement validateRequestMiddleware for user retrieval endpoint

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -2,6 +2,8 @@ import { getAllUsersController } from "@/controllers/user.controller.js";
 import asyncHandler from "@/middlewares/async-handler.middleware.js";
 import { authenticateMiddleware } from "@/middlewares/authenticate.middleware.js";
 import { apiLimitter } from "@/middlewares/rate-limit.middleware.js";
+import validateRequestMiddleware from "@/middlewares/validate-request.middleware.js";
+import { getAllUserSchema } from "@/validator/user.validator.js";
 import { Router } from "express";
 
 const router: Router = Router();
@@ -11,6 +13,7 @@ router
   .get(
     apiLimitter,
     authenticateMiddleware(["admin", "teacher"]),
+    validateRequestMiddleware(getAllUserSchema),
     asyncHandler(getAllUsersController),
   );
 
