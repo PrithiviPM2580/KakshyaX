4cbf26aa2a5ebb2684c132f8c9384813532fd8ee
Prithivi <prithivi@users.noreply.github.com>
2026-01-18 05:30:41 +0545

    Implement user retrieval by ID: add route and controller for fetching user details

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,9 +1,15 @@
-import { getAllUsersController } from "@/controllers/user.controller.js";
+import {
+  getAllUsersController,
+  getUserByIdController,
+} from "@/controllers/user.controller.js";
 import asyncHandler from "@/middlewares/async-handler.middleware.js";
 import { authenticateMiddleware } from "@/middlewares/authenticate.middleware.js";
 import { apiLimitter } from "@/middlewares/rate-limit.middleware.js";
 import validateRequestMiddleware from "@/middlewares/validate-request.middleware.js";
-import { getAllUserSchema } from "@/validator/user.validator.js";
+import {
+  getAllUserSchema,
+  getUserByIdSchema,
+} from "@/validator/user.validator.js";
 import { Router } from "express";
 
 const router: Router = Router();
@@ -17,4 +23,12 @@ router
     asyncHandler(getAllUsersController),
   );
 
+router
+  .route("/:id")
+  .get(
+    apiLimitter,
+    authenticateMiddleware(["admin", "teacher"]),
+    validateRequestMiddleware(getUserByIdSchema),
+    asyncHandler(getUserByIdController),
+  );
 export default router;
