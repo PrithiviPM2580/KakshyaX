870262ed5b8baa36bae9a810977030109c9dc5e7
Prithivi <prithivi@users.noreply.github.com>
2026-01-17 18:53:49 +0545

    Add authentication middleware: implement authenticateMiddleware for session validation and role-based access control

---
+++ backend/src/middlewares/authenticate.middleware.ts
@@ -0,0 +1,39 @@
+import { auth } from "@/lib/auth.lib.js";
+import APIError from "@/lib/api-error.lib.js";
+import type { NextFunction, Request, Response } from "express";
+import { logger } from "better-auth";
+
+export function authenticateMiddleware(allowRoles: Roles[]) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const session = await auth.api.getSession({
+        headers: Object.fromEntries(
+          Object.entries(req.headers).map(([key, value]) => [
+            key,
+            String(value),
+          ]),
+        ),
+      });
+
+      if (!session) {
+        logger.info("No session found");
+        return next(new APIError(401, "Unauthorized"));
+      }
+
+      if (!allowRoles.includes(session.user.role as Roles)) {
+        logger.info(`User role ${session.user.role} not allowed`);
+        return next(new APIError(403, "Forbidden"));
+      }
+
+      req.session = {
+        user: {
+          id: session.user.id,
+          role: session.user.role as Roles,
+        },
+      };
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+}
